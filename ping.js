d78243cc524694c18e3f1cf4fc4ac3ea3879a9a0
Vitor Fukumoto <vitor_fuku@hotmail.com>
2025-03-27 18:37:07 -0300

    code updatd to automatically ping a specific telegram channel with a good morning anime gif

--- ping.js
+++ ping.js
@@ -8,22 +8,39 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
 
 bot.command('stop', (ctx) => {
-    ctx.reply('Bot parado!');
-    bot.stop();
-  });
+  ctx.reply('Bot parado!');
+  bot.stop();
+});
 
+bot.hears('eita', async (ctx) => { ctx.reply('eita o que, porra') });
 
-bot.hears('eita', async (ctx) => {ctx.reply ('eita o que, porra')});
+async function currentTime() {
+  const today = new Date();
+  const hours = String(today.getHours()).padStart(2, '0');
+  const minutes = String(today.getMinutes()).padStart(2, '0');
+  const seconds = String(today.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+}
 
-bot.hears(/bom dia/i, async (ctx) => {
-    try {
-        const response = await axios.get('https://nekos.best/api/v2/yawn');
-        const gifUrl = response.data.results[0].url;
-        await ctx.replyWithAnimation(gifUrl,{caption:'/ᐠ - ˕ -マ   oyahooo!'});
-    } catch (error) {
-      console.error('Erro:', error);
-      ctx.reply('Não consegui buscar a imagem no momento.');
-    }
-  });
+async function morning() {
+  try {
+    const response = await axios.get('https://nekos.best/api/v2/yawn');
+    const gifUrl = response.data.results[0].url;
+    bot.telegram.sendAnimation(process.env.CHAT_ID, gifUrl, { caption: '/ᐠ - ˕ -マ oyahooo!' });
+  } catch (error) {
+    console.error('Erro:', error);
+    return "Não consegui buscar a imagem no momento"
+  }
+}
 
-  bot.launch();
+setInterval(async () => {
+  const now = await currentTime();
+  if (now === "07:30:00") {
+    const ctx = await bot.telegram.getChat(process.env.CHAT_ID);
+    await morning(ctx);
+  } else {
+    console.log("Ainda não é hora de dar bom dia");
+  }
+}, 1000);;
+
+bot.launch();
